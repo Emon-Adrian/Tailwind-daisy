06e70ccc57f97c6beb9f5831cbd5821ff7c4f4ff
Emon-Adrian <matovuemon20@gmail.com>
2022-04-01 20:25:16 +0300

    fun

---
+++ src/Components/Divider.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Divider = () => {
+  return (
+    <div>
+          <div class="flex flex-col w-full lg:flex-row mt-3">
+  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia nisi, adipisci facilis laudantium voluptate quo repellendus dolorum deserunt. Corrupti quidem, beatae odit numquam eveniet officia at perferendis minima dicta quisquam!</div> 
+  <div class="divider lg:divider-horizontal">OR</div> 
+  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam nostrum labore vitae voluptate, sed reprehenderit! Odio aliquid neque delectus, beatae minus accusantium iusto. Deserunt repudiandae distinctio eius impedit fuga aliquid.</div>
+</div>
+  
+    </div>
+  )
+}
+
+export default Divider
